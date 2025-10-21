684fb9ca502181ce0f3abf9db51e03cbae062385
Salim Bahassou <sbahasso@student.codam.nl>
2025-10-21 20:25:41 +0200

    Update ft_printf.h with new function prototypes
    Added new function prototypes for printing various types.

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sabahass <sabahass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 22:26:43 by sabahass          #+#    #+#             */
-/*   Updated: 2025/09/17 23:15:28 by sabahass         ###   ########.fr       */
+/*   Updated: 2025/10/21 19:10:24 by sabahass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,16 @@
 # include <stdarg.h>
 # include <stdint.h>
 # include <unistd.h>
+# include <stdio.h>
+# include <limits.h>
+# include "libft/libft.h"
 
 int	ft_printf(const char *format, ...);
+int	ft_print_c(int c);
+int	ft_print_s(const char *s);
+int	ft_print_d(int n);
+int	ft_print_u(unsigned int n);
+int	ft_print_x(unsigned long n, int upper);
+int	ft_print_p(const void *p);
 
 #endif
